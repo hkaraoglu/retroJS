bfba86cea9824cd46556e2f9498cadd803c4c711
Hasan KARAOĞLU <rhasank1994@gmail.com>
2017-12-23 23:49:40 +0300

    .

--- service.js
+++ service.js
@@ -8,7 +8,7 @@ class Service
     // example POST service
     login(email, password, callback)
     {
-      this.request.start("POST", "login", {"email" : email, "password" : password, "password" : password }, callback, true);
+      this.request.start("POST", "auth/login", {"email" : email, "password" : password, "password" : password }, callback, true);
     }
 
     //example GET serviceUrl
