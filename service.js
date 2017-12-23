916c4f4af811552517e4165496984928f322fa73
Hasan KARAOĞLU <rhasank1994@gmail.com>
2017-12-23 23:48:44 +0300

    callback.js was encapsulated. getRespone function in Service Class was moved to callback.js and renamed to start in a new Call which named Request.

--- service.js
+++ service.js
@@ -2,46 +2,18 @@ class Service
 {
     constructor(serviceUrl)
     {
-        this.serviceUrl = serviceUrl;
+        this.request = new Request(serviceUrl);
     }
 
     // example POST service
     login(email, password, callback)
     {
-      this.getResponse("POST", "auth/login", {"email" : email, "password" : password, "password" : password }, callback, true);
+      this.request.start("POST", "login", {"email" : email, "password" : password, "password" : password }, callback, true);
     }
 
     //example GET serviceUrl
     forgotten(email, callback)
     {
-      this.getResponse("GET", "auth/forgotten?email=" + email , "", callback, true);
+      this.request.start("GET", "auth/forgotten?email=" + email , "", callback, true);
     }
-
-    getResponse(type, path, parameters, callback, async)
-  	{
-  		var data = "";
-  		if(type == 'POST')
-  		{
-  			data = parameters;
-  		}
-  		$.ajax({
-  			type: type,
-  			cache: false,
-  			async: async,
-  			xhrFields: {
-  				withCredentials: true
-  			},
-  			url: this.serviceUrl + "/" + path,
-  			data:data,
-  			success: function(result, status, xhr)
-  			{
-  				callback.onSuccess(result);
-        },
-  			error: function(xhr)
-  			{
-          callback.onError(xhr.message);
-        },
-      });
-  	}
-
 }
